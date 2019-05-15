5840e299e887187dd717853c1d806fe715313c63
Ubaid Usmani <usmaniubaid@gmail.com>
2019-05-15 17:07:09 +0530

    code updated

--- Code/review_tool1.py
+++ Code/review_tool1.py
@@ -1,11 +1,5 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file 'Review_tool.ui'
-#
-# Created by: PyQt5 UI code generator 5.10
-#
-# WARNING! All changes made in this file will be lost!
-
 from PyQt5 import QtCore, QtGui, QtWidgets
 
 class Ui_MainWindow(object):
