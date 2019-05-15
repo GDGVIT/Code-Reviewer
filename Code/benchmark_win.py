f8aa6652203f80d52df9ec1a23b1547cbf8b9f6b
Ubaid Usmani <usmaniubaid@gmail.com>
2019-05-15 17:07:43 +0530

    Ui_code updated

--- Code/benchmark_win.py
+++ Code/benchmark_win.py
@@ -1,10 +1,5 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file 'benchmark_win.ui'
-#
-# Created by: PyQt5 UI code generator 5.10
-#
-# WARNING! All changes made in this file will be lost!
 
 from PyQt5 import QtCore, QtGui, QtWidgets
 
