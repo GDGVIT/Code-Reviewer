d085454a0b97533c795fe997fa4a050b91f9b215
SaurusXI <vermashantanu@hotmail.com>
2020-08-15 16:17:30 +0530

    delete old source after move

--- Code/etsa.py
+++
@@ -1,5 +0,0 @@
-from pylint import epylint as lint
-from io import StringIO
-import numpy
-(pylint_stdout, pylint_stderr) = lint.py_run('test.py', return_std=True)
-print(StringIO.read(pylint_stdout))
