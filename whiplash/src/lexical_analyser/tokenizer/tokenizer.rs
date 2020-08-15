dcbb2b7b11020502621c9dd95e6a3c341d04c7ff
SaurusXI <vermashantanu@hotmail.com>
2020-08-16 01:57:58 +0530

    bugfix: split_lexeme was not appending last lexeme to result; fixed.

---
+++ whiplash/src/lexical_analyser/tokenizer/tokenizer.rs
@@ -0,0 +1,127 @@
+// use crate::lexical_analyser::token::{token::*, tokentype::*};
+
+
+/// Tokenizer for Python
+pub struct Tokenizer<'a> {
+    pub keywords: Vec<&'a str>,
+    pub operators: Vec<&'a str>,
+    pub symbols_to_ignore: Vec<char>,
+    pub symbols_to_include: Vec<&'a str>,
+    pub parenthesis: Vec<&'a str>,
+}
+
+impl<'a> Tokenizer<'a> {
+
+    pub fn is_literal(s: &&str) -> bool {
+        
+        if (s.starts_with("\"") || 
+            s.starts_with("b\"") || 
+            s.starts_with("r\"") || 
+            s.starts_with("u\""))
+            && s.ends_with("\"") {
+                return true;
+
+        } else if (s.starts_with("\'") || 
+            s.starts_with("b\'") || 
+            s.starts_with("r\'") || 
+            s.starts_with("u\'"))
+            && s.ends_with("\'") {
+                return true;
+
+        }
+
+        false
+    }
+
+    /// Instantiates a Tokenizer
+    pub fn new() -> Tokenizer<'a> {
+        // List of keywords used in Python. Can be found by doing keyword.kwlist in py
+        let keywords = vec![ 
+            "None", 
+            "and", 
+            "as", 
+            "assert", 
+            "async", 
+            "await", 
+            "break", 
+            "class", 
+            "continue", 
+            "def", 
+            "del", 
+            "elif", 
+            "else", 
+            "except", 
+            "finally", 
+            "for", 
+            "from", 
+            "global", 
+            "if", 
+            "import", 
+            "in", 
+            "is", 
+            "lambda", 
+            "nonlocal", 
+            "not", 
+            "or", 
+            "pass", 
+            "raise", 
+            "return", 
+            "try", 
+            "while", 
+            "with", 
+            "yield"
+        ];
+
+        // List of operators in Python
+        let operators = vec![
+            "+",
+            "-",
+            "*",
+            "/",
+            "//",
+            "&",
+            "^",
+            "~",
+            "|",
+            "**",
+            "<<",
+            ">>",
+            "%",
+            "<",
+            ">",
+            "=",
+            "<=",
+            ">=",
+            "==",
+            "!=",
+        ];
+
+        // List of symbols
+        let symbols_to_ignore = vec![
+            ',',
+        ];
+
+        let parenthesis = vec![
+            "(",
+            ")",
+            "{",
+            "}",
+            "[",
+            "]",
+        ];
+
+        let symbols_to_include = vec![
+            ":",
+            "\'\'\'",
+        ];
+
+        Tokenizer {
+            keywords, 
+            operators, 
+            symbols_to_ignore, 
+            symbols_to_include, 
+            parenthesis
+        }
+    }
+    
+}
