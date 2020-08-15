b796f2ef34155a949185d00b1625d084da4c69cd
SaurusXI <vermashantanu@hotmail.com>
2020-08-15 16:12:25 +0530

    add multi-line file reading in example execution in main

--- whiplash/src/main.rs
+++ whiplash/src/main.rs
@@ -1,12 +1,18 @@
 use crate::tokenizer::tokenizer::Tokenizer;
 use std::fs;
+use std::io::{self, prelude::*};
 
 mod tokenizer;
 mod ast;
 
-fn main() {
-    let contents = fs::read_to_string("../test1.txt").unwrap();
-
+fn main() -> io::Result<()> {
+    let file = fs::File::open("../test1.txt")?;
+    let reader = io::BufReader::new(file);
     let tokenizer = Tokenizer::new();
-    println!("{:#?}", tokenizer.tokenize_line(&contents[..]));
+
+    for line in reader.lines() {
+        println!("{:#?}", tokenizer.tokenize_line(&line?[..]));
+    }
+
+    Ok(())
 }
