90135b3fc5eecc9e6c89b6c499807e5b5fd88055
SaurusXI <vermashantanu@hotmail.com>
2020-08-19 15:10:49 +0530

    specify grammar for parser

--- whiplash/src/parser/ast/ast.rs
+++ whiplash/src/parser/ast/ast.rs
@@ -15,6 +15,31 @@ use crate::lexical_analyser::tokenizer::tokenizer::Tokenizer;
 /// and_test: not_test ('and' not_test)*
 /// not_test: 'not' not_test | comparison
 /// comparison: expr (comp_op expr)*
+/// comp_op: '<'|'>'|'=='|'>='|'<='|'<>'|'!='|'in'|'not' 'in'|'is'|'is' 'not'
+/// expr: xor_expr ('|' xor_expr)*
+/// xor_expr: and_expr ('^' and_expr)*
+/// and_expr: shift_expr ('&' shift_expr)*
+/// shift_expr: arith_expr (('<<'|'>>') arith_expr)*
+/// arith_expr: term (('+'|'-') term)*
+/// term: factor (('*'|'@'|'/'|'%'|'//') factor)*
+/// factor: ('+'|'-'|'~') factor | power
+/// power: atom_expr ['**' factor]
+/// atom_expr: [AWAIT] atom trailer*
+/// atom: ('(' [yield_expr|testlist_comp] ')' |
+///     '[' [testlist_comp] ']' |
+///     '{' [dictorsetmaker] '}' |
+///     NAME | NUMBER | STRING+ | '...' | 'None' | 'True' | 'False')
+/// testlist_comp: (namedexpr_test|star_expr) ( comp_for | (',' (namedexpr_test|star_expr))* [','] )
+/// trailer: '(' [arglist] ')' | '[' subscriptlist ']' | '.' NAME
+/// subscriptlist: subscript (',' subscript)* [',']
+/// subscript: test | [test] ':' [test] [sliceop]
+/// sliceop: ':' [test]
+/// exprlist: (expr|star_expr) (',' (expr|star_expr))* [',']
+/// testlist: test (',' test)* [',']
+/// dictorsetmaker: ( ((test ':' test | '**' expr)
+///                    (comp_for | (',' (test ':' test | '**' expr))* [','])) |
+///                   ((test | star_expr)
+///                    (comp_for | (',' (test | star_expr))* [','])) )
 /// lambdef: 'lambda' [varargslist] ':' test
 /// testlist: test (',' test)* [',']
 /// star_expr: '*' expr
@@ -27,6 +52,30 @@ use crate::lexical_analyser::tokenizer::tokenizer::Tokenizer;
 /// raise_stmt: 'raise' [test ['from' test]]
 /// yield_expr: 'yield' [yield_arg]
 /// yield_arg: 'from' test | testlist_star_expr
+/// namedexpr_test: test [':=' test]
+/// arglist: argument (',' argument)*  [',']
+/// argument: ( test [comp_for] |
+///     test ':=' test |
+///     test '=' test |
+///     '**' test |
+///     '*' test )
+/// comp_iter: comp_for | comp_if
+/// sync_comp_for: 'for' exprlist 'in' or_test [comp_iter]
+/// comp_for: [ASYNC] sync_comp_for
+/// comp_if: 'if' test_nocond [comp_iter]
+/// test_nocond: or_test | lambdef_nocond
+/// lambdef_nocond: 'lambda' [varargslist] ':' test_nocond
+/// varargslist: vfpdef ['=' test ](',' vfpdef ['=' test])* ',' '/' [',' [ (vfpdef ['=' test] (',' vfpdef ['=' test])* [',' [
+///     '*' [vfpdef] (',' vfpdef ['=' test])* [',' ['**' vfpdef [',']]]
+///     | '**' vfpdef [',']]]
+///     | '*' [vfpdef] (',' vfpdef ['=' test])* [',' ['**' vfpdef [',']]]
+///     | '**' vfpdef [',']) ]] | (vfpdef ['=' test] (',' vfpdef ['=' test])* [',' [
+///     '*' [vfpdef] (',' vfpdef ['=' test])* [',' ['**' vfpdef [',']]]
+///     | '**' vfpdef [',']]]
+///     | '*' [vfpdef] (',' vfpdef ['=' test])* [',' ['**' vfpdef [',']]]
+///     | '**' vfpdef [',']
+///     )
+/// vfpdef: NAME
 
 struct AST {
     root: Node,
@@ -39,3 +88,5 @@ impl AST {
     }
 
 }
+
+// namedexpr_test, arglist, comp_for 
