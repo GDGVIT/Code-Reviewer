59bcc96fd3cd4d1cf13f789dfded9a1e772ca6d2
SaurusXI <vermashantanu@hotmail.com>
2020-09-08 19:43:13 +0530

    add methods to traverse a generated parse tree and extract grammar rules from it

--- whiplash/src/grammar/reader/parse_tree/grammatize.rs
+++ whiplash/src/grammar/reader/parse_tree/grammatize.rs
@@ -1,23 +1,67 @@
 // Converts a generated parse tree into a grammar
 use super::{Tree, Node, NodeType};
-use crate::grammar::production::{Atom, Atoms};
+use crate::grammar::production::{Atoms, Rule, Atom};
+use crate::parser::node;
 
 impl Tree {
-    pub fn to_rhs_list(current_node: Box<Node>) -> Vec<Atoms> {
-        let out = vec![];
+    pub fn get_rules(self, start_symbol: node::NodeType) -> Vec<Rule> {
+        let mut out = vec![];
+        let rhs_list = Self::to_rhs_list(self.root);
+
+        for rhs in rhs_list.into_iter() {
+            out.push(
+                Rule::from(start_symbol.clone(), rhs)
+            );
+        }
+
+        out
+    }
+
+    fn to_rhs_list(current_node: Box<Node>) -> Vec<Atoms> {
+        let mut out = vec![];
         for child in current_node.children.into_iter() {
+            let is_separator = format!("{:?}", child.atoms) == format!("{:?}", Atoms::from_single_token("|".to_string()));
+
             if let NodeType::X = child.nodetype {
                 out = Self::permute_concat(out, Self::to_rhs_list(child));
-            } else if format!("{:?}", child.atoms.vals) == "| ".to_string() {
+            } else if is_separator {
                 continue;
             } else {
                 out = Self::permute_concat(out, vec![child.atoms]);
             }
-        } 
+        }
+
         out
     }
 
-    fn permute_concat(accumulated: Vec<Atoms>, tails: Vec<Atoms>) -> Vec<Atoms> {
-        vec![]
+    fn permute_concat(accumulated: Vec<Atoms>, adjuncts: Vec<Atoms>) -> Vec<Atoms> {
+        let mut result: Vec<Atoms> = vec![];
+
+        for prependee in &accumulated {
+            for adjunct in &adjuncts {
+                // Check if adjunct or prependee are epsilon
+                let is_adjunct_epsilon = format!("{:?}", adjunct) == format!("{:?}", Atoms::from_single_atom(Atom::Epsilon));
+                let is_prependee_epsilon = format!("{:?}", prependee) == format!("{:?}", Atoms::from_single_atom(Atom::Epsilon));
+
+                if !is_adjunct_epsilon && !is_prependee_epsilon {
+                    result.push(Atoms::from(
+                        [&prependee.vals[..], &adjunct.vals[..]].concat()
+                    ));
+                    
+                } else if !is_prependee_epsilon {
+                    result.push(Atoms::from(prependee.vals));
+                    result.push(Atoms::from(vec![]));
+
+                } else if !is_adjunct_epsilon {
+                    result.push(Atoms::from(adjunct.vals));
+                    result.push(vec![]);
+
+                } else {
+                    result.push(Atoms::from(vec![]));
+                }
+            }
+        }
+        
+        result
     }
 }
