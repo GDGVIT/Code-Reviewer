c49405b0df93fd2646c685f8444dee66831addab
SaurusXI <vermashantanu@hotmail.com>
2020-09-08 20:01:42 +0530

    add modules to main

--- whiplash/src/main.rs
+++ whiplash/src/main.rs
@@ -1,13 +1,14 @@
-use crate::lexical_analyser::tokenizer::tokenizer::Tokenizer;
-use std::fs;
-use std::io::{self, prelude::*};
-
 extern crate strum;
 #[macro_use]
 extern crate strum_macros;
 
+use crate::lexical_analyser::tokenizer::tokenizer::Tokenizer;
+use std::fs;
+use std::io::{self, prelude::*};
+
 mod lexical_analyser;
 mod parser;
+mod grammar;
 
 fn main() -> io::Result<()> {
     let file = fs::File::open("../test1.py")?;
